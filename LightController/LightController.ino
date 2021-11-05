b3a28d134a1e9a94696450fd786a7c4317dbb20a
Sascha Vis <saschavis@gmail.com>
2021-11-05 23:05:05 +0100

    Added wifi to the sketch

--- LightController/LightController.ino
+++ LightController/LightController.ino
@@ -1,9 +1,10 @@
-#include <WiFi.h>
-#include <ArduinoHA.h>
+#include <WiFiManager.h>
 
 // Libraries
 #include "Switch.h"
 #include "Light.h"
+
+WiFiManager wifiManager;
 
 //Switch definitions
 Switch switch1("Badkamer Knop");
@@ -11,9 +12,26 @@ Light light1("Badkamer Lamp");
 
 
 void setup(){
+    WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP    
+    
     Serial.begin(115200);
     delay(50); //to let the serial monitor open correctly
-    Serial.println("Starting lightController v1.0...");
+    
+    Serial.println("Starting lightController v1.1...");
+
+    //reset settings - wipe credentials for testing
+    // wifiManager.resetSettings();
+
+    wifiManager.setConfigPortalBlocking(false);
+
+    //automatically connect using saved credentials if they exist
+    //If connection fails it starts an access point with the specified name
+    if(wifiManager.autoConnect("LightControl")){
+        Serial.println("connected...yeey :)");
+    }
+    else {
+        Serial.println("Unable to connect to wifi. Config portal is running...");
+    }
 
     //Attach Lights
     light1.attach(17);
@@ -33,5 +51,7 @@ void setup(){
 
 void loop(){
     switch1.handle();
+
+    wifiManager.process();
     delay(1000);
 }
